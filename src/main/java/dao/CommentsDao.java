14e6edd47e9c2efa67fb9062c49f684e59527ed2
BarakaIan <ianb.makale@gmail.com>
2021-03-10 08:42:09 +0300

    delete the APIExceptions file

--- src/main/java/dao/CommentsDao.java
+++ src/main/java/dao/CommentsDao.java
@@ -5,5 +5,7 @@ import models.Comments;
 import java.util.List;
 
 public interface CommentsDao {
-    
+    void add(Comments comments);
+    List<Comments> getAll();
+    Comments findById(int id);
 }
