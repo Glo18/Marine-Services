63fb3dc08a7c6f8b23638d756ba49b50b42b865d
BarakaIan <ianb.makale@gmail.com>
2021-03-10 13:37:19 +0300

    add the Comments.java file

--- src/main/java/exceptions/ApiException.java
+++ src/main/java/exceptions/ApiException.java
@@ -10,5 +10,5 @@ public class ApiException extends RuntimeException {
 
     public int getStatusCode() {
         return statusCode;
-    } 
+    }
 }
--- src/main/java/models/Comments.java
+++ src/main/java/models/Comments.java
@@ -1,4 +1,43 @@
 package models;
 
+import java.util.Objects;
+
 public class Comments {
+
+    private int id;
+    private String comment;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Comments(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comments comment = (Comments) o;
+        return id == comment.id &&
+                comment.equals(comment.comment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, comment);
+    }
 }
