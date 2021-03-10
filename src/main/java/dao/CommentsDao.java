f2acfefa14cbd330658cfd1d6cf8efc88002c18e
BarakaIan <ianb.makale@gmail.com>
2021-03-10 08:39:55 +0300

    add the CommentsDao file

--- src/main/java/dao/CommentsDao.java
+++ src/main/java/dao/CommentsDao.java
@@ -5,7 +5,5 @@ import models.Comments;
 import java.util.List;
 
 public interface CommentsDao {
-    void add(Comments comments);
-    List<Comments> getAll();
-    Comments findById(int id);
+    
 }
