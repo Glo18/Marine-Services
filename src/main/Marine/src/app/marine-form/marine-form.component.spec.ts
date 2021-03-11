b2032bf72eb8a59957feb42a64af1a2e94a84dc4
Glo18 <kiogoraspecial@gmail.com>
2021-03-11 06:12:29 +0300

    form

---
+++ src/main/Marine/src/app/marine-form/marine-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MarineFormComponent } from './marine-form.component';
+
+describe('MarineFormComponent', () => {
+  let component: MarineFormComponent;
+  let fixture: ComponentFixture<MarineFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MarineFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MarineFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/main/Marine/src/app/marine-form/marine-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Marine } from '../marine';
+
+@Component({
+  selector: 'app-marine-form',
+  templateUrl: './marine-form.component.html',
+  styleUrls: ['./marine-form.component.css']
+})
+export class MarineFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
