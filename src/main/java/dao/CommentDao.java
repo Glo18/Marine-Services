b145f3ca97ebfab15d9738a4cd064757b01bde7a
BarakaIan <ianb.makale@gmail.com>
2021-03-12 12:38:43 +0300

    add the changes to file

---
+++ src/main/java/dao/CommentDao.java
@@ -0,0 +1,11 @@
+//package dao;
+//
+//import models.Comment;
+//
+//import java.util.List;
+//
+//public interface CommentDao {
+//    void add(Comment comment);
+//    List<Comment> getAll();
+//    Comment findById(int id);
+//}
