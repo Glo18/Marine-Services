54c30cf009e1ce3da924604d525628ac8d37b5fa
BarakaIan <ianb.makale@gmail.com>
2021-03-12 12:40:00 +0300

    add the changes to files

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,8 +1,8 @@
-//import org.sql2o.Sql2o;
-//
-//public class DB {
-//
-//
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/marine_services", "moringa", "D2000sep");
-//
-//}
+import org.sql2o.Sql2o;
+
+public class DB {
+
+
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/marine_services", "moringa", "D2000sep");
+
+}
--- src/main/java/dao/Sql2oCommentDao.java
+++ src/main/java/dao/Sql2oCommentDao.java
@@ -1,46 +1,46 @@
-//package dao;
-//
-//import models.Comment;
-//import org.sql2o.Connection;
-//import org.sql2o.Sql2o;
-//import org.sql2o.Sql2oException;
-//
-//import java.util.List;
-//
-//public class Sql2oCommentDao implements CommentDao {
-//    private final Sql2o sql2o;
-//    public Sql2oCommentDao(Sql2o sql2o) {
-//        this.sql2o=sql2o;
-//    }
-//    @Override
-//    public void add(Comment comment) {
-//        String sql = "INSERT INTO comments (comment) VALUES (:comment)";
-//        try (Connection con = sql2o.open()) {
-//            int id = (int) con.createQuery(sql, true)
-//                    .bind(comment)
-//                    .executeUpdate()
-//                    .getKey();
-//            comment.setId(id);
-//        } catch (Sql2oException ex) {
-//            System.out.println(ex);
-//        }
-//    }
-//
-//    @Override
-//    public List<Comment> getAll() {
-//        String sql = "SELECT * FROM comments";
-//        try(Connection con = sql2o.open()) {
-//            return con.createQuery(sql).executeAndFetch(Comment.class);
-//        }
-//    }
-//
-//    @Override
-//    public Comment findById(int id) {
-//        try (Connection con = sql2o.open()) {
-//            return con.createQuery("SELECT * FROM comments WHERE id = :id")
-//                    .addParameter("id", id)
-//                    .executeAndFetchFirst(Comment.class);
-//        }
-//    }
-//
-//}
+package dao;
+
+import models.Comment;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oCommentDao implements CommentDao {
+    private final Sql2o sql2o;
+    public Sql2oCommentDao(Sql2o sql2o) {
+        this.sql2o=sql2o;
+    }
+    @Override
+    public void add(Comment comment) {
+        String sql = "INSERT INTO comments (comment) VALUES (:comment)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(comment)
+                    .executeUpdate()
+                    .getKey();
+            comment.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Comment> getAll() {
+        String sql = "SELECT * FROM comments";
+        try(Connection con = sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Comment.class);
+        }
+    }
+
+    @Override
+    public Comment findById(int id) {
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM comments WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Comment.class);
+        }
+    }
+
+}
